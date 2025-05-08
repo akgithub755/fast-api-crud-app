b688e357eff974cbad66eb3e6e7f0e586ed68ecd
akgithub755 <akgithub755@gmail.com>
2025-05-08 17:43:28 +0530

    add routes

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+
+@app.get('/')
+def root():
+    return {'message':'welcome to my api'}
+
+
+@app.get('/posts')
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post('/posts')
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
